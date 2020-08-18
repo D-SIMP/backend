1106f94b3166a0706bebf7f9285ed0b1aeeb34c6
Aldrich Neil <aldrich.neil.hung@gmail.com>
2020-08-18 16:36:56 +0700

    updated sql

--- simp.sql
+++ simp.sql
@@ -16,6 +16,61 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `follow`
+--
+
+DROP TABLE IF EXISTS `follow`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `follow` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userid` int NOT NULL,
+  `followid` int DEFAULT NULL,
+  `followfrom` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `follow`
+--
+
+LOCK TABLES `follow` WRITE;
+/*!40000 ALTER TABLE `follow` DISABLE KEYS */;
+INSERT INTO `follow` VALUES (1,1,1,'a few seconds ago'),(2,1,2,'a few seconds ago'),(3,1,3,'a few seconds ago'),(4,2,1,'a few seconds ago');
+/*!40000 ALTER TABLE `follow` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `posts` (
+  `postid` int NOT NULL AUTO_INCREMENT,
+  `author` int NOT NULL,
+  `caption` varchar(45) DEFAULT '',
+  `imageURL` varchar(100) NOT NULL,
+  `createdAt` varchar(45) DEFAULT '0000-00-00 00:00:00',
+  `followers` int DEFAULT '0',
+  `following` int DEFAULT '0',
+  PRIMARY KEY (`postid`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (1,1,'1st post','/images/SIMP1597743064693.jpg','a few seconds ago',0,0),(2,1,'2nd post','/images/SIMP1597743070865.jpg','a few seconds ago',0,0),(3,2,'1st post','/images/SIMP1597743078827.jpg','a few seconds ago',0,0);
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `users`
 --
 
@@ -31,7 +86,7 @@ CREATE TABLE `users` (
   `status` varchar(45) DEFAULT 'offline',
   `verified` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`userid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +95,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'user','f839ff8ac14f77b31002485a09aa48297d547fda029ade6d06b63217511a26a0','aldrich.neil.hung@gmail.com','null','offline',0);
+INSERT INTO `users` VALUES (1,'user','f839ff8ac14f77b31002485a09aa48297d547fda029ade6d06b63217511a26a0','aldrich.neil.hung@gmail.com','null','offline',0),(2,'cok','f839ff8ac14f77b31002485a09aa48297d547fda029ade6d06b63217511a26a0','hahaha@gmail.com','null','offline',0);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -57,4 +112,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-08 11:11:21
+-- Dump completed on 2020-08-18 16:36:10
